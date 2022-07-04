d88b7ab6e92f923c6bca8a824e353a111f2a98da
Cade Coleman <cae828@gmail.com>
2022-07-04 17:08:41 -0500

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,3 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
 // Questions and answer key stored as an object for each question 
 
 //  create a function that starts the game with an addeventlistener click on a button
@@ -20,211 +8,187 @@
 
 // create right/wrong conditional statement that answers your response, counts your right/wrong questions, and subtracts or leaves alone time
 
+
+
+// HTML buttons and tags
 var questionText = document.querySelector("#game-question")
 var gameStarter = document.querySelector("#gameStarter");
 var button1 = document.querySelector("#button-1");
 var button2 = document.querySelector("#button-2");
 var button3 = document.querySelector("#button-3");
 var button4 = document.querySelector("#button-4");
-var ans = document.querySelector('#ans');
-;
+var score = document.getElementById('score');
+var timer = document.getElementById('timer');
+var screenStart = document.getElementsByClassName('.start-screen');
+var gameScreen = document.getElementsByClassName('gameScreen');
+ var timeLeft = 45;
+// 
+var qCounter = 0;
+
+// score that is to be displayed at the end of the game
+
+
+var score = 0;
 
 
 
-
-// Display Object strings on buttons
-
-
-
-
+// Objects with questions and answers
 var questionKey1 = {
-    A: 'The Force',
-    B: 'The Way',
-    C: 'The Holy Spirit', 
-    D: 'Duralock',
-    Q: 'In Star Wars, what do they call the invisible power that binds the galaxy together?',
+    answer: 'The Force',
+    choices: ['The Force', 'The Way','The Holy Spirit','Duralock'],
+    question: 'In Star Wars, what do they call the invisible power that binds the galaxy together?',
 };
 
 var questionKey2 = {
-    A: 'Jedi',
-    B:'Wookies',
-    C:'Bothans',
-    D: 'Clones',
-    Q:'What species stole the plans to the Death Star?',
+    answer: 'Bothans',
+    choices: ['Wookies', 'Jedi','Clones', 'Bothans'],
+    question: 'What species stole the plans to the Death Star?',
 };
 
 var questionKey3 = {
-    A: 'Yoda',
-    B: 'Syfo Dyas',
-    C: 'Mace Windu',
-    D: 'Luke SKywalker',
-    Q: 'Who was the Jedi master that ordered to create clones?',
+
+    Answer:'Syfo Dyas',
+    Choices: ['Yoda','Mace Windu','Luke SKywalker','Syfo Day'],
+    Question:'Who was the Jedi master that ordered to create clones?',
 };
 
 var questionKey4 = {
-    A: 'Twilek',
-    B: 'Hutt',
-    C: 'Mon Calamari',
-    D: 'Son Malamari',
-    Q: ' What alien race does Admiral Ackbar belong to?',
+    answer: 'Mon Calamari',
+    choices: ['Hutt','Jedi','Son Malamari','Mon Calamari'],
+    question: ' What alien race does Admiral Ackbar belong to?',
 };
 
 var questionKey5 = {
-    A: 'Call of the Wild',
-    B: 'Ancient Steel',
-    C: 'Clone Trooper',
-    D: 'Mandalorian',
-    Q: 'What is the battle armor used by Boba Fett?',
+    answer: 'Mandalorian',
+    choices: ['Call of the Wild', 'Delorean', 'Clone Trooper','Mandalorian',],
+    question: 'What is the battle armor used by Boba Fett?',
 };
 
 var questionKey6 = {
-    A: 'Megaannom cruiser',
-    B: 'Millenium Falcon',
-    C: 'Aluminium Eagle', 
-    D: 'Hans Solo', 
-    Q: 'What is the name of Han Solo’s ship?',
+    answer: 'Millenium Falcon',
+    choices: ['Megaannom cruiser','Millenium Falcon','Aluminium Eagle', 'Hans Solo',], 
+    question: 'What is the name of Han Solo’s ship?',
 };
 
 var questionKey7 = {
-    A: 'George Lucas',
-    B: 'George R.R. Martin',
-    C: 'J. R. R. Tolkien', 
-    D: 'Gene Roddenberry',
-    Q: 'Who is the creator of Star Wars?',
+    answer: 'George Lucas',
+     choices: ['George Lucas', 'George R.R. Martin','J. R. R. Tolkien', 'Gene Roddenberry',],
+    question: 'Who is the creator of Star Wars?',
 };
 
 var questionKey8 = {
-    A: 'Coruscant',
-    B: 'Tatooine',
-    C: 'Hoth', 
-    D: 'Kashyyyk',
-    Q: 'Where did Obi-Wan take Luke after his birth?',
+    answer: 'Tatooine',
+    choices: ['Coruscant','Tatooine', 'Hoth', 'Kashyyyk'],
+    question: 'Where did Obi-Wan take Luke after his birth?',
 };
 
 var questionKey9 = {
-    A: '1982',
-    B: '1980',
-    C: '1996', 
-    D: '1977',
-    Q: 'What year did the first Star Wars movie come out?',
+    answer: '1977',
+    choices: ['1982', '1980','1996', '1977'],
+    questions: 'What year did the first Star Wars movie come out?',
 };
 
 var questionKey10 = {
-    A: 'Darth Sidious',
-    B: 'Darth vader',
-    C: 'Anakin Skywalker', 
-    D: 'Count Duko',
-    Q: 'Who killed Mace Windu?',
+    answer: 'Darth Sidious',
+    choices: ['Darth Sidious','Darth vader','Anakin Skywalker','Count Duko'],
+    question: 'Who killed Mace Windu?',
 };
 
 
 
+var questionArray = [questionKey1,questionKey2,questionKey3, questionKey4, questionKey5, questionKey6, questionKey7,questionKey8, questionKey9, questionKey10];
+console.log(questionArray);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// This appends object children to html buttons Q1
-gameStarter.addEventListener('click', function(){
-    button1.append(questionKey1.A);
-    button2.append(questionKey1.B);
-    button3.append(questionKey1.C);
-    button4.append(questionKey1.D);
-    questionText.append(questionKey1.Q)
-})
-
-// // This appends object children to html buttons Q2
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey2.A);
-//     button3.append(questionKey2.C);
-//     button4.append(questionKey2.D);
-//     button2.append(questionKey2.B);
-//     questionText.append(questionKey2.Q)
-// })
-
-// // This appends object children to html buttons Q3
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey3.A);
-//     button2.append(questionKey3.B);
-//     button3.append(questionKey3.C);
-//     button4.append(questionKey3.D);
-//     questionText.append(questionKey3.Q)
-// })
-
-// // This appends object children to html buttons Q4
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey4.A);
-//     button2.append(questionKey4.B);
-//     button3.append(questionKey4.C);
-//     button4.append(questionKey4.D);
-//     questionText.append(questionKey4.Q)
-// })
-
-// // This appends object children to html buttons Q5
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey5.A);
-//     button2.append(questionKey5.B);
-//     button3.append(questionKey5.C);
-//     button4.append(questionKey5.D);
-//     questionText.append(questio0nKey5.Q)
-// })
-
-// // This appends object children to html buttons Q6
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey6.A);
-//     button2.append(questionKey6.B);
-//     button3.append(questionKey6.C);
-//     button4.append(questionKey6.D);
-//     questionText.append(questionKey6.Q)
-// })
-
-// // This appends object children to html buttons Q7
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey7.A);
-//     button2.append(questionKey7.B);
-//     button3.append(questionKey7.C);
-//     button4.append(questionKey7.D);
-//     questionText.append(questionKey7.Q)
-// })
-
-// // This appends object children to html buttons Q8
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey8.A);
-//     button2.append(questionKey8.B);
-//     button3.append(questionKey8.C);
-//     button4.append(questionKey8.D);
-//     questionText.append(questionKey8.Q)
-// })
-
-// // This appends object children to html buttons Q9
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey9.A);
-//     button2.append(questionKey9.B);
-//     button3.append(questionKey9.C);
-//     button4.append(questionKey9.D);
-//     questionText.append(questionKey9.Q)
-// })
-
-// // This appends object children to html buttons Q10
-// gameStarter.addEventListener('click', function(){
-//     button1.append(questionKey10.A);
-//     button2.append(questionKey10.B);
-//     button3.append(questionKey10.C);
-//     button4.append(questionKey10.D);
-//     questionText.append(questionKey10.Q);
-// })
  
 
-//  Object  QUESTION 6
-//  Object  QUESTION 7
-//  Object  QUESTION 8
+
+// Timer that counts down from 5
+function countdown() {
+   
+
+    
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timer` to show the remaining seconds
+        timer.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timer.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timer` to an empty string
+        timer.textContent = 'Game Over';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // this will dynamically change the screen to the score board
+        showScore();
+      }
+    }, 1000);
+}
+
+
+  function showScore(){
+    var newDiv = document.createElement("div");
+    newDiv.textContent('Your score is ' + score);
+  }
+
+
+
+// Function to check the correct answer 
+function checkAnswer(event){
+    console.log(event);
+    //  Reads the name of the item clicked
+    var ansClick = event.target.textContent;
+    var correctAnswer = questionArray[qCounter].answer; 
+    if(ansClick === correctAnswer){
+
+    //subtract time
+    console.log("yes");
+    qCounter++;
+    score = score + 10;
+ 
+    displayQ();
+}else{
+  
+    qCounter++;
+    timeLeft = timeLeft - 5;
+    displayQ();
+}}
+
+
+
+
+
+
+// Changes texts on buttons
+function displayQ(){
+    button1.textContent = questionArray[qCounter].choices[0];
+    button2.textContent = questionArray[qCounter].choices[1];
+    button3.textContent = questionArray[qCounter].choices[2];
+    button4.textContent = questionArray[qCounter].choices[3];
+
+  
+    questionText.textContent = questionArray[qCounter].question;    
+    
+}
+
+
+// This button starts the game using an addEventListener
+gameStarter.addEventListener('click', function(){
+    displayQ();
+    countdown();
+
+
+})
+
+//  This adds functionality to the buttons.
+button1.addEventListener('click', checkAnswer)
+button2.addEventListener('click', checkAnswer)
+button3.addEventListener('click', checkAnswer)
+button4.addEventListener('click', checkAnswer)
+ 
